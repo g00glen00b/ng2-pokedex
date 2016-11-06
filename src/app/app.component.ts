7436ca03f8f5978942870cf13c4b55c206691463
g00glen00b <me@g00glen00b.be>
2016-11-06 19:20:44 +0100

    Removed test code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,8 @@
-import { Component } from '@angular/core';
-import {PokemonService} from './shared/services/pokemon.service';
+import {Component} from '@angular/core';
 
 @Component({
-  providers: [PokemonService],
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app works!';
-
-
-  constructor(service: PokemonService) {
-    service.findAll(0, 10).subscribe(pokemons => console.log(pokemons));
-    service.findOne(1).subscribe(pokemon => console.log(pokemon));
-  }
 }
