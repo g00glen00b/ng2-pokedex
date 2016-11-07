bfa0b8316fe831cad40db1cc691f672a47c84735
g00glen00b <me@g00glen00b.be>
2016-11-07 23:08:17 +0100

    Implemented ngOnInit() lifecycle hook

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PokemonService} from '../shared/services/pokemon.service';
+import {PokemonEntry} from '../shared/models/pokemon-entry';
 
 @Component({
+  providers: [PokemonService],
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html'
 })
 export class PokemonListComponent implements OnInit {
+  pokemons: PokemonEntry[];
+  count: number = 0;
+  offset: number = 0;
+  limit: number = 20;
 
-  constructor() { }
+  constructor(private _service: PokemonService) { }
 
   ngOnInit() {
+    this.findAll(this.offset, this.limit);
   }
 
+  findAll(offset: number, limit: number) {
+    this.pokemons = [];
+    this._service.findAll(offset, limit).subscribe(result => {
+      this.pokemons = result.pokemons;
+      this.count = result.count;
+    });
+  }
 }
