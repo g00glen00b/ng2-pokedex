af126eb9c32d99db6689dc7b21115e35270463a9
g00glen00b <me@g00glen00b.be>
2016-11-19 20:44:13 +0100

    Implemented navbar component

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,9 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
-  
+  @Input() title: String;
 }
