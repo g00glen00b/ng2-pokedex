4b2c360c169cf4e64bac8288df47d4ac940e7158
g00glen00b <me@g00glen00b.be>
2016-11-19 21:03:12 +0100

    Implemented subscribing to route parameters

--- src/app/pokemon-info/pokemon-info.component.ts
+++ src/app/pokemon-info/pokemon-info.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {PokemonService} from '../shared/services/pokemon.service';
+import {Pokemon} from '../shared/models/pokemon';
 
 @Component({
+  providers: [PokemonService],
   selector: 'app-pokemon-info',
   templateUrl: './pokemon-info.component.html'
 })
 export class PokemonInfoComponent implements OnInit {
+  pokemon: Pokemon;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute, private _service: PokemonService) { }
 
   ngOnInit() {
+    this._route.params
+      .map(params => params['id'])
+      .flatMap(id => this._service.findOne(id))
+      .subscribe(pokemon => this.pokemon = pokemon);
   }
 
 }
