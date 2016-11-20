f2ea56758a5595dba9dbc35121738f5cf146cfd9
g00glen00b <me@g00glen00b.be>
2016-11-20 18:58:03 +0100

    Implemented title reset to Pokédex when changing routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router, NavigationStart} from '@angular/router';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private _titleService: Title, private _router: Router) {
+
+  }
+
+  ngOnInit() {
+    this._router.events
+      .filter(event => event instanceof NavigationStart)
+      .subscribe(event => this._titleService.setTitle('Pokédex'));
+  }
 }
