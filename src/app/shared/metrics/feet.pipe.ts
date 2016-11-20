55fae0ee2385c83227242d20ad9d88855d922f46
g00glen00b <me@g00glen00b.be>
2016-11-20 19:54:49 +0100

    Implemented feet pipe

--- src/app/shared/metrics/feet.pipe.ts
+++ src/app/shared/metrics/feet.pipe.ts
@@ -1,12 +1,28 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as _ from 'lodash';
 
 @Pipe({
   name: 'feet'
 })
 export class FeetPipe implements PipeTransform {
+  private _types = { 'cm': 0.01, 'dm': 0.1, 'm': 1 };
+  private _feetPerMeter: number = 3.28084;
+  private _inchesPerFeet: number = 12;
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: number, type: string): string {
+    let meters = this.getMeters(value, type),
+        feet = meters * this._feetPerMeter,
+        roundedFeet = Math.floor(feet),
+        inches = Math.round((feet - roundedFeet) * this._inchesPerFeet);
+    return `${roundedFeet}' ${_.padStart(inches.toString(), 2, '0')}"`;
   }
 
+  getMeters(value: number, type: string): number {
+    let conversion = this._types[type];
+    if (conversion == null) {
+       throw new Error('Could not find type');
+    } else {
+      return value * conversion;
+    }
+  }
 }
