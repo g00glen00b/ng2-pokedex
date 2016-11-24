b4b42f6e96aa40a632b68da12e47faf36f3f59a7
g00glen00b <me@g00glen00b.be>
2016-11-24 22:48:39 +0100

    Implemented animations

--- src/app/pokemon-info/pokemon-description/pokemon-description.component.ts
+++ src/app/pokemon-info/pokemon-description/pokemon-description.component.ts
@@ -1,10 +1,40 @@
-import {Component, Input, OnInit, OnChanges} from '@angular/core';
+import {Component, Input, OnInit, OnChanges, animate, style, transition, trigger, state} from '@angular/core';
 import {PokemonDescription} from '../../shared/models/pokemon-description';
 
 @Component({
   selector: 'app-pokemon-description',
   templateUrl: './pokemon-description.component.html',
-  styleUrls: ['./pokemon-description.component.css']
+  styleUrls: ['./pokemon-description.component.css'],
+  animations: [
+    trigger('descriptionState', [
+      state('Alpha Sapphire', style({ backgroundColor: '#304ffe' })),
+      state('Omega Ruby', style({ backgroundColor: '#b71c1c' })),
+      state('Y', style({ backgroundColor: '#1e88e5' })),
+      state('X', style({ backgroundColor: '#b71c1c' })),
+      state('White 2', style({ backgroundColor: '#ffccbc' })),
+      state('Black 2', style({ backgroundColor: '#263238', color: '#eceff1' })),
+      state('White', style({ backgroundColor: '#cfd8dc' })),
+      state('Black', style({ backgroundColor: '#212121', color: '#eceff1' })),
+      state('Soulsilver', style({ backgroundColor: '#b0bec5' })),
+      state('Heartgold', style({ backgroundColor: '#ff7043' })),
+      state('Platinum', style({ backgroundColor: '#673ab7', color: '#ede7f6' })),
+      state('Diamond', style({ backgroundColor: '#1a237e', color: '#e8eaf6' })),
+      state('Pearl', style({ backgroundColor: '#b388ff' })),
+      state('Firered', style({ backgroundColor: '#ff5722' })),
+      state('Leafgreen', style({ backgroundColor: '#9ccc65' })),
+      state('Emerald', style({ backgroundColor: '#66bb6a' })),
+      state('Ruby', style({ backgroundColor: '#b71c1c' })),
+      state('Sapphire', style({ backgroundColor: '#1565c0', color: '#e3f2fd' })),
+      state('Gold', style({ backgroundColor: '#ffab00' })),
+      state('Silver', style({ backgroundColor: '#757575' })),
+      state('Crystal', style({ backgroundColor: '#c5cae9' })),
+      state('Red', style({ backgroundColor: '#d50000' })),
+      state('Blue', style({ backgroundColor: '#0d47a1', color: '#e3f2fd' })),
+      state('Yellow', style({ backgroundColor: '#fdd835' })),
+      transition('void => *', [style({opacity: 0, backgroundColor: '*'}), animate('500ms ease-in', style({opacity: 1}))]),
+      transition('* => *', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class PokemonDescriptionComponent implements OnInit, OnChanges {
   @Input() descriptions: PokemonDescription[];
